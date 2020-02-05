27293fbb149a93cb27509db698bb8dac8fd552b8
Guillaume Englert <genglert at hybird.org@localhost>
2020-02-05 18:07:20 +0100

    refs #46 #30 Fix the blocks of the Credentials form. It worked anyway, but the semantic was broken.

--- creme/creme_config/forms/user_role.py
+++ creme/creme_config/forms/user_role.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2019  Hybird
+#    Copyright (C) 2009-2020  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -147,7 +147,7 @@ class CredentialsGeneralStep(CremeModelForm):
 
     blocks = FieldBlockManager(
         ('general', _('General information'), '*'),
-        ('actions', _('Actions'), [*PERM_FIELDS.values()]),
+        ('actions', _('Actions'), [*PERM_FIELDS.keys()]),
     )
 
     class Meta:
