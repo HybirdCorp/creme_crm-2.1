6c754608f32100d7ab78d4c05e0e9f82f00308d1
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2020-04-06 15:52:05 +0200

    refs #43 Version is now "2.1.3".

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     zip_safe=False,
     long_description=README,
     install_requires=[
-        "django>=2.2.11,<2.2.1024",
+        "django>=2.2.12,<2.2.1024",
         "redis>=3.3.11,<3.3.1024",
         "Pillow>=6.2.2,<6.2.1024",
         "python-dateutil>=2.8.1,<2.8.1024",
