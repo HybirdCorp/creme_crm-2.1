42fc9a1d78585cea9d028c4c79d317c95da87f51
MrJmad Aka Jean-Michel <354064+mrjmad@users.noreply.github.com>
2020-05-07 19:22:39 +0200

    refs #6 #26 Fix several bugs in the emails synchronisation. Some encoding errors. Some badly tested reworked code for attachments.

--- creme/emails/crudity_register.py
+++ creme/emails/crudity_register.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2019  Hybird
+#    Copyright (C) 2009-2020  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -88,7 +88,7 @@ class EntityEmailBackend(CrudityBackend):
                                   object_entity=mail, user_id=current_user_id,
                                  )
         create_doc = partial(Document.objects.create,
-                             user_id=current_user_id, folder=folder,
+                             user_id=current_user_id, linked_folder=folder,
                              description=_('Received with the mail {}').format(mail),
                             )
 
