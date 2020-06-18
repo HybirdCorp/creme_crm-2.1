4715837e1961f741cae30876ec09148e5b1fe2b7
Guillaume Englert <genglert@hybird.org>
2020-06-18 14:17:57 +0200

    The field 'emails.MailingList.description' was missing in the forms.

--- creme/emails/forms/mailing_list.py
+++ creme/emails/forms/mailing_list.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2019  Hybird
+#    Copyright (C) 2009-2020  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -38,8 +38,8 @@ Organisation  = persons.get_organisation_model()
 
 class MailingListForm(CremeEntityForm):
     class Meta:
-        model  = MailingList
-        fields = ('user', 'name')
+        model = MailingList
+        fields = ('user', 'name', 'description')
 
 
 class AddContactsForm(CremeForm):
