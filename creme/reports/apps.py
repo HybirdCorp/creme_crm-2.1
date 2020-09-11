a323420a31e1bacfa6a3f45b8d8b4c4a564973a0
Guillaume Englert <genglert@hybird.org>
2020-09-11 18:46:45 +0200

    Remove an invalid field name in an exclusion list.

--- creme/reports/apps.py
+++ creme/reports/apps.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2015-2019  Hybird
+#    Copyright (C) 2015-2020  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -63,9 +63,11 @@ class ReportsConfig(CremeAppConfig):
         from .forms.bulk import ReportFilterBulkForm
 
         register = bulk_update_registry.register
-        register(self.Report, exclude=['ct', 'columns'],
-                 innerforms={'filter': ReportFilterBulkForm},
-                )
+        register(
+            self.Report,
+            exclude=['ct'],  # 'columns'
+            innerforms={'filter': ReportFilterBulkForm},
+        )
         register(self.ReportGraph, exclude=['days', 'is_count', 'chart'])  # TODO: chart -> innerform
 
     def register_icons(self, icon_registry):
