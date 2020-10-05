385b87e9e4512614ee4f8742478f907d06700e6b
Guillaume Englert <genglert@hybird.org>
2020-10-05 09:48:06 +0200

    Fix the use of a deprecated import (python 3.7+).

--- creme/creme_core/views/bricks.py
+++ creme/creme_core/views/bricks.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2017-2019  Hybird
+#    Copyright (C) 2017-2020  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-from collections import Iterator
+from collections.abc import Iterator
 from json import loads as json_load
 import logging
 
