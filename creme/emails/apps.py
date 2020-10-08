14b4a80e5c2de840a7136f557fa374c9deac955a
Guillaume Englert <genglert@hybird.org>
2020-10-08 14:22:57 +0200

    These fields of 'EmailTemplate' cannot be inner-edited anymore: 'body', 'body_html'.

--- creme/emails/apps.py
+++ creme/emails/apps.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2019  Hybird
+#    Copyright (C) 2009-2020  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -87,6 +87,7 @@ class EmailsConfig(CremeAppConfig):
         register(self.MailingList, exclude=('children', 'contacts', 'organisations'))
         register(self.EmailCampaign, exclude=('mailing_lists',))
         register(models.EmailSending, exclude=('sender', 'type', 'sending_date'))  # TODO: tags modifiable=False ??
+        register(self.EmailTemplate, exclude=('body', 'body_html'))
         register(self.EntityEmail, exclude=('sender', 'recipient', 'subject',
                                             'body', 'body_html', 'signature', 'attachments',
                                            )  # TODO: idem
