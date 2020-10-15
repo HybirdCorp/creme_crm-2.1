32e942d5d2f6c191557053f6e6353d2b85ec8001
Fabre Florian <ffabre@hybird.org>
2020-10-15 14:50:04 +0200

    Refs #107 Upgrade to tinymce 3.4.9 without jquery adapter. Create the widget 'creme.widget.Editor' as remplacement.

--- creme/creme_core/forms/widgets.py
+++ creme/creme_core/forms/widgets.py
@@ -881,7 +881,7 @@ class OptionalSelect(OptionalWidget):
 
 class TinyMCEEditor(widgets.Textarea):
     def get_context(self, name, value, attrs):
-        widget_type = 'ui-creme-jqueryplugin'
+        widget_type = 'ui-creme-editor'
         context = super().get_context(name=name, value=value, attrs=attrs)
 
         final_attrs = context['widget']['attrs']
@@ -889,23 +889,6 @@ class TinyMCEEditor(widgets.Textarea):
                     'ui-creme-input ui-creme-widget '
         final_attrs['class'] = css_class + widget_type
         final_attrs['widget'] = widget_type
-        final_attrs['plugin'] = 'tinymce'
-        final_attrs['plugin_options'] = json_dump({
-            'mode':                            'textareas',
-            'script_url':                      '{}tiny_mce/tiny_mce.js'.format(settings.MEDIA_URL),
-            'convert_urls':                    False,
-            'theme':                           'advanced',
-            'height':                          300,
-            'plugins':                         "spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template, fullpage",
-            'theme_advanced_buttons1':         "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-            'theme_advanced_buttons2':         "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-            'theme_advanced_buttons3':         "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-            'theme_advanced_buttons4':         "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,blockquote,pagebreak,|,insertfile,insertimage",
-            'theme_advanced_toolbar_location': 'top',
-            'theme_advanced_toolbar_align':    'left',
-            'theme_advanced_path_location':    'bottom',
-            'theme_advanced_resizing':         True,
-        })
 
         return context
 
